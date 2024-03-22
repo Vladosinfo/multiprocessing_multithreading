6aa745f535973e1d9065dd74fabdc98e5a66db34
Vlad <vladyslav.pasko@gmail.com>
2024-03-22 19:45:59 +0200

    First commit )

---
+++ multiprocessing_multithreading/multiprocessing_multithreading/main.py
@@ -0,0 +1,188 @@
+import sys
+import os
+import shutil
+import glob
+import time
+from pathlib import Path
+from threading import Thread
+# from concurrent.futures import ThreadPool
+
+# Constants
+IMAGES = 'images'
+VIDEOS = 'videos'
+DOCUMENTS = 'documents'
+AUDIO = 'audio'
+ARCHIVES = 'archives'
+FILES_ACCORDING_CATEGORY = 'files_according_category'
+KNOWN_EXTENSIONS = 'known_extensions'
+UNKNOWN_EXTENSIONS = 'unknown_extensions'
+UNKNOWN = 'unknown'
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ~`!@#$%^&*()-=+<>?\",.\'\\[]{}№;/:| "
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g", "_", "_", "_", "_", "_", "_",
+                "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_",
+                "_", "_", "_", "_", "_", "_", "")
+TRANSLATE_DICT = {}  
+
+result = {
+    FILES_ACCORDING_CATEGORY: {
+        IMAGES: [],
+        VIDEOS: [],
+        DOCUMENTS: [],
+        AUDIO: [],
+        ARCHIVES: [],
+        UNKNOWN: []
+    },
+    KNOWN_EXTENSIONS: [],
+    UNKNOWN_EXTENSIONS: []
+}
+
+sort_folders = {
+    IMAGES: ['JPEG', 'PNG', 'JPG', 'SVG'],
+    VIDEOS: ['AVI', 'MP4', 'MOV', 'MKV'],
+    DOCUMENTS: ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'],
+    AUDIO: ['MP3', 'OGG', 'WAV', 'AMR'],
+    ARCHIVES: ['ZIP', 'GZ', 'TAR']
+}
+
+
+def translate_dict_create():
+    for c , t in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+        TRANSLATE_DICT[ord(c)] = t
+        TRANSLATE_DICT[ord(c.upper())] = t.upper()
+
+
+def normalize(file_name):
+    return file_name.translate(TRANSLATE_DICT)
+
+
+def create_folders(path, folder):
+    if not os.path.exists(path+folder):
+        os.makedirs(path+folder)
+
+
+def rename_file(path, file_name, file_extension):
+    file_spl = file_name.removesuffix('.'+file_extension)
+    renamed_fname = normalize(file_spl)
+    new_fname = renamed_fname+'.'+file_extension
+    os.rename(path+file_name, path+new_fname)
+    #print('Symbols of file name isn\'t cyrillic')
+    return new_fname
+  
+
+def archive_process(main_patn, path, fname):
+    unpack_folder = fname.split('.')
+    try:
+        shutil.unpack_archive(path+fname, main_patn+unpack_folder[0])
+        os.remove(path+fname)
+    except:
+        print(f'Сouldn\'t open the archive - {path+fname}')
+        os.remove(path+fname)
+    
+
+def sort_files(MAIN_PATH_SORT, path, file_name, file_extension):
+    moved = 0
+    renamed_fname = rename_file(path, file_name, file_extension)
+    for folder, extensions in sort_folders.items():
+            if str(file_extension).lower() in str(extensions).lower():
+                create_folders(MAIN_PATH_SORT, folder)
+                result[FILES_ACCORDING_CATEGORY][folder].append(renamed_fname)
+                if not file_extension in result[KNOWN_EXTENSIONS]:
+                    result[KNOWN_EXTENSIONS].append(file_extension)
+                # Archive processing
+                if folder == ARCHIVES:
+                    archive_process(MAIN_PATH_SORT+folder+'/', path, renamed_fname)
+                    moved = 1
+                    break
+                shutil.move(path+renamed_fname, MAIN_PATH_SORT+folder+'/'+renamed_fname)
+                moved = 1
+                break
+    if moved == 0:
+        create_folders(MAIN_PATH_SORT, UNKNOWN)
+        result[FILES_ACCORDING_CATEGORY][UNKNOWN].append(renamed_fname)
+        if not file_extension in result[UNKNOWN_EXTENSIONS]:
+            result[UNKNOWN_EXTENSIONS].append(file_extension)
+        shutil.move(path+renamed_fname, MAIN_PATH_SORT+UNKNOWN+'/'+renamed_fname)
+
+
+
+def folders_iteration_by_thread(list_files_for_remove, MAIN_PATH_SORT, i):
+
+    except_url = str(i).replace('\\', '/')
+
+    if (i.is_file() and except_url.find(MAIN_PATH_SORT+IMAGES) < 0 and except_url.find(MAIN_PATH_SORT+VIDEOS) < 0 
+        and except_url.find(MAIN_PATH_SORT+DOCUMENTS) < 0 and except_url.find(MAIN_PATH_SORT+AUDIO) < 0
+        and except_url.find(MAIN_PATH_SORT+ARCHIVES) < 0 and except_url.find(MAIN_PATH_SORT+UNKNOWN) < 0):
+        file_array = i.name.split('.')
+        files_path = str(i.parent)+'\\'
+        sort_files(MAIN_PATH_SORT, files_path, i.name, file_array[-1])
+
+        folder_path = str(i.parent)+'\\*.*'
+        if len(glob.glob(folder_path))  == 0:
+            list_files_for_remove.append(files_path)    
+
+
+
+def folders_iteration(MAIN_PATH_SORT, path):
+
+    p = Path(path)
+    list_files_for_remove = []
+
+    # with ThreadPool.ThreadPoolExecutor(max_workers=2) as executor:
+    for i in p.glob('**/*'):
+        # except_url = str(i).replace('\\', '/')
+        if i.is_dir() and (len(os.listdir(str(i))) == 0):
+            i.rmdir()
+            continue
+        
+        folders_iteration_by_thread(list_files_for_remove, MAIN_PATH_SORT, i)
+        thread_folders = Thread(target=folders_iteration_by_thread, args=(list_files_for_remove, MAIN_PATH_SORT, i,))
+        thread_folders.start()
+
+        # if (i.is_file() and except_url.find(MAIN_PATH_SORT+IMAGES) < 0 and except_url.find(MAIN_PATH_SORT+VIDEOS) < 0 
+        #     and except_url.find(MAIN_PATH_SORT+DOCUMENTS) < 0 and except_url.find(MAIN_PATH_SORT+AUDIO) < 0
+        #     and except_url.find(MAIN_PATH_SORT+ARCHIVES) < 0 and except_url.find(MAIN_PATH_SORT+UNKNOWN) < 0):
+        #     file_array = i.name.split('.')
+        #     files_path = str(i.parent)+'\\'
+        #     sort_files(MAIN_PATH_SORT, files_path, i.name, file_array[-1])
+
+        #     folder_path = str(i.parent)+'\\*.*'
+        #     if len(glob.glob(folder_path))  == 0:
+        #         list_files_for_remove.append(files_path)
+
+    # Remove empty folders
+    for fold in list_files_for_remove:
+        if os.path.exists(fold):
+            shutil.rmtree(fold)
+
+
+def main_process(path):
+    start = time.time()
+
+    MAIN_PATH_SORT = path
+
+    translate_dict_create()
+    thread_dict_create = Thread(target=translate_dict_create)
+    thread_dict_create.start()
+    thread_dict_create.join()
+    
+    folders_iteration(MAIN_PATH_SORT,path)
+
+    finish = time.time()
+    res = finish - start
+    res_mil = res * 1000
+    print(f"Timing of program: {res}")
+    print(f"Timing of program mil: {res_mil}")
+
+    return result
+
+
+def main():
+    main_process("d:/temp/temp/ds/")
+    # main_process(sys.argv[1])
+
+
+if __name__ == '__main__':
+    # if len(sys.argv) < 2:
+    #     sys.exit(1)
+    main()
